2f627b3c98afb7b8fb1961332be8746d5a50b205
vaibhav <62588796+Vaibhav123v@users.noreply.github.com>
2020-08-19 02:01:50 +0530

    Add files via upload

---
+++ youtubeola.py
@@ -0,0 +1,267 @@
+import turtle
+t = turtle.Turtle()
+t.speed(0)
+turtle.bgcolor("black")
+t.color("white")
+t.up()
+t.goto(-80,50)
+t.down()
+t.fillcolor("white")
+t.begin_fill()
+t.forward(230)
+t.setheading(270)
+s = 360
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+
+    
+t.forward(180)
+s = 270
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+
+t.forward(230)
+s = 180
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+
+t.forward(180)
+s = 90
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+    
+t.end_fill()
+t.up()
+t.forward(110)
+t.setheading(270)
+#t.color("red")
+t.forward(140)
+t.forward(100)
+t.setheading(0)
+t.down
+t.fillcolor("black")
+t.begin_fill()
+t.circle(100)
+t.end_fill()
+t.setheading(90)
+t.up()
+t.forward(100)
+t.forward(-50)
+t.setheading(0)
+t.down()
+t.fillcolor("#9ACD32")
+t.begin_fill()
+t.circle(50)
+t.end_fill()
+t.up()
+t.setheading(270)
+t.forward(45)
+t.setheading(0)
+t.down()
+t.pensize(3)
+t.color("white")
+c = 100
+for i in range(2):
+    t.circle(c)
+    t.penup()
+    t.forward(5)
+    t.pendown()
+t.up()
+
+
+###############################################you tube coding######################################
+t.goto(-60,280)
+t.color("white")
+t.fillcolor("red")
+
+t.begin_fill()
+t.down()
+t.forward(250)
+t.setheading(270)
+s = 360
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+
+    
+t.forward(100)
+s = 270
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+
+t.forward(250)
+s = 180
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+
+t.forward(100)
+s = 90
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+t.end_fill()
+t.up()
+t.forward(70)
+t.setheading(270)
+t.forward(40)
+t.down()
+t.fillcolor("white")
+t.begin_fill()
+t.forward(130)
+t.setheading(0)
+t.setheading(30)
+t.forward(130)
+t.setheading(150)
+t.forward(130)
+t.end_fill()
+t.setheading(0)
+
+##############################netflix#############################
+t.up()
+t.goto(-400,50)
+t.down()
+t.fillcolor("black")
+t.begin_fill()
+t.forward(200)
+t.setheading(270)
+s = 360
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+
+    
+t.forward(180)
+s = 270
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+
+t.forward(200)
+s = 180
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+
+t.forward(180)
+s = 90
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+t.forward(30)    
+t.end_fill()
+t.up()
+t.color("black")
+t.setheading(270)
+t.forward(240)
+t.setheading(0)
+t.down()
+t.color("red")
+t.fillcolor("#E50914")
+t.begin_fill()
+t.forward(30)
+t.setheading(90)
+t.forward(180)
+t.setheading(180)
+t.forward(30)
+t.setheading(270)
+t.forward(180)
+t.end_fill()
+t.setheading(0)
+t.up()
+t.forward(75)
+t.down()
+t.color("red")
+t.fillcolor("#E50914")
+t.begin_fill()
+t.forward(30)
+t.setheading(90)
+t.forward(180)
+t.setheading(180)
+t.forward(30)
+t.setheading(270)
+t.forward(180)
+t.end_fill()
+t.color("red")
+t.fillcolor("red")
+t.begin_fill()
+t.setheading(113)
+t.forward(195)
+t.setheading(0)
+t.forward(31)
+t.setheading(293)
+t.forward(196)
+t.end_fill()
+t.setheading(0)
+######################################WHATS APP############################
+t.up()
+t.goto(350,50)
+t.down()
+t.fillcolor("#00FF00")
+t.begin_fill()
+t.forward(200)
+t.setheading(270)
+s = 360
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+
+    
+t.forward(180)
+s = 270
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+
+t.forward(200)
+s = 180
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+
+t.forward(180)
+s = 90
+for i in range(9):
+    s = s - 10
+    t.setheading(s)
+    t.forward(10)
+t.end_fill()
+t.up()
+t.forward(70)
+t.setheading(270)
+t.forward(80)
+t.down()
+t.fillcolor("white")
+t.begin_fill()
+t.forward(130)
+t.setheading(0)
+t.setheading(30)
+t.forward(130)
+t.setheading(150)
+t.forward(130)
+t.end_fill()
+
+
+
+
+    
